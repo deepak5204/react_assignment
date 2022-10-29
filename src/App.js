4852ce0da53042dfb1fdbcbc8f9727fc18cde086
Deepak <deepakkumar750206@gmail.com>
2022-10-29 17:00:21 +0530

    fetch data from api and show data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Home from "./component/Home";
 
 function App() {
+  const [values, setValues] = useState([]);
+
+  useEffect(() => {
+    async function getUsers() {
+      const url = "http://demo2211087.mockable.io/mock";
+
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+        body: JSON.stringify({}),
+      });
+
+      const data = await response.json();
+      setValues(data.companies);
+    }
+
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>
+        <Home data={values} />
+       
+      </h2>
     </div>
   );
 }
---
+++ src/component/Home.js
@@ -0,0 +1,22 @@
+import "../App.css"
+
+function Home(props) {
+      const  {data } = props
+      console.log(data);
+      return (
+        <div> 
+            {
+                data.map((item, index)=>
+                <div className="itemBox">
+                    <li className="list">{item.name}</li>
+                    <li className="list">{item.email}</li>
+                    <li className="list">{item.status}</li>
+                </div>
+                )
+            }  
+        </div>
+      );
+    }
+    
+    export default Home;
+    
